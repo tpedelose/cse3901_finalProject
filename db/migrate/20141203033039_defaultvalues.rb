ae9de2363f9f828cb2058d0c113a23acd9a923e5
ThomasCarlin <ThomasJeffersonCarlin@gmail.com>
2014-12-05 11:44:43 -0500

    kk

--- db/migrate/20141203033039_defaultvalues.rb
+++ db/migrate/20141203033039_defaultvalues.rb
@@ -1,8 +1,5 @@
 class Defaultvalues < ActiveRecord::Migration
   def change
-   	change_column_default :players, :count, 0
-  	change_column_default :players, :score, 0
-  	change_column_default :players, :num_people_fooled, 0
 
   	change_column_default :questions, :times_used, 0
   	change_column_default :questions, :times_correct, 0
